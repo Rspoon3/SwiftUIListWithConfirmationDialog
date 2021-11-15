6505d8e382648b338ab567df24b656c7624d829a
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-15 09:48:43 -0500

    • Added broken example

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SwiftUIListWithConfirmationDialog
+Demonstrates how a confirmation dialog does not work with items in a list on iPad.
+
+**System Info:**
+* Xcode 13.1 (13A1030d)
+* iPad Pro 11 (2018, iOS 15.1)
+
+
+**Reproduction steps:**
+1. Run app on iPad in landscape
+2. Swipe to delete to show the confirmation dialog
+
+
+**Actual Results:**
+The confirmation dialog randomly appears on any item.
+
+
+**Expected Results:**
+The confirmation dialog should appear right next to the swiped row.
--- SwiftUIListWithConfirmationDialog/ContentView.swift
+++ SwiftUIListWithConfirmationDialog/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var confirmationShown = false
+    @State private var numbers = Array(0..<20)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(numbers, id: \.self){ i in
+                    NavigationLink("This is row \(i)") {
+                        Text("This is row \(i)")
+                    }
+                    .swipeActions {
+                        Button(role: .destructive) {
+                            confirmationShown.toggle()
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    }
+                    .confirmationDialog("Are you sure?",
+                                        isPresented: $confirmationShown,
+                                        titleVisibility: .visible) {
+                        Button("Delete", role: .destructive) {
+                            numbers.remove(at: i)
+                        }
+                        .keyboardShortcut(.defaultAction)
+                    } message: {
+                        Text("This action cannot be undone")
+                    }
+                }
+            }
+            .navigationTitle("Sidebar")
+            
+            Text("Hello, World!")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
