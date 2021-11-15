5a62b094cd72945dd6fa7a8c0899671bd74d9571
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2021-11-15 09:19:57 -0500

    Initial Commit

---
+++ SwiftUIListWithConfirmationDialog/SwiftUIListWithConfirmationDialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListWithConfirmationDialogApp.swift
+//  SwiftUIListWithConfirmationDialog
+//
+//  Created by Richard Witherspoon on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListWithConfirmationDialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
